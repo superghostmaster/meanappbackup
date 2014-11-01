aabeb443f96a22ddeaed592576a62885e69974df
GhostRed <chrisashaffer@gmail.com>
2014-11-01 21:44:33 +0000

    -a

---
+++ public/modules/articles/config/articles.client.config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Configuring the Articles module
+angular.module('articles').run(['Menus',
+	function(Menus) {
+		// Set top bar menu items
+		Menus.addMenuItem('topbar', 'Consultool', 'articles', 'dropdown', '/articles(/create)?');
+		Menus.addSubMenuItem('topbar', 'articles', 'List Consulting Questions', 'articles');
+		Menus.addSubMenuItem('topbar', 'articles', 'Create New Consulting Question', 'articles/create');
+	}
+]);
